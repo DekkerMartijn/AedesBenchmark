b4bf50a8479fa51bcf07b01db1a7b95d8a697ef1
Martijn Dekker <dekkermartijn@hotmail.com>
2022-01-06 09:25:29 +0100

    V1.2

--- 210211 Data Preparation.R
+++ 210211 Data Preparation.R
@@ -44,9 +44,16 @@ setwd("~/Trivire/R/Git/AedesBencmark/AedesBenchmark/Data")
 Df_algemene_kenmerken <- read.csv('Algemene Kenmerken  - Woningcorporaties.csv', sep = ';', stringsAsFactors = T)
 Df_algemene_kenmerken[Df_algemene_kenmerken==""]<-NA #replace empty string with NA
 Df_algemene_kenmerken <- droplevels.data.frame(Df_algemene_kenmerken) #Drop empty levels
+Df_algemene_kenmerken$Gewogen.verhuureenheden..aantal...2019. <-as.numeric(Df_algemene_kenmerken$Gewogen.verhuureenheden..aantal...2019.)
+Df_algemene_kenmerken$Zelfstandige.huurwoningen.DAEB..aantal...2019. <-as.numeric(Df_algemene_kenmerken$Zelfstandige.huurwoningen.DAEB..aantal...2019.)
+Df_algemene_kenmerken$Onzelfstandige.overige.wooneenheden.DAEB..aantal...2019. <-as.numeric(Df_algemene_kenmerken$Onzelfstandige.overige.wooneenheden.DAEB..aantal...2019.)
+Df_algemene_kenmerken$Zelfstandige.huurwoningen.niet.DAEB..aantal...2019. <-as.numeric(Df_algemene_kenmerken$Zelfstandige.huurwoningen.niet.DAEB..aantal...2019.)
+Df_algemene_kenmerken$Zelfstandige.huurwoningen.niet.DAEB..aantal...2019. <-as.numeric(Df_algemene_kenmerken$Zelfstandige.huurwoningen.niet.DAEB..aantal...2019.)
+Df_algemene_kenmerken$FTE..aantal...2019. <-as.numeric(Df_algemene_kenmerken$FTE..aantal...2019.)
+
 
 # Huurderoordeel
-Df_Huurderoordeel <- read.csv('Huurdersoordeel  - Woningcorporaties 2017_2020.csv', sep = ';', stringsAsFactors = T)
+Df_Huurderoordeel <- read.csv('Huurdersoordeel  - Woningcorporaties 2017_2021.csv', sep = ';', stringsAsFactors = T)
 Df_Huurderoordeel[Df_Huurderoordeel==""]<-NA #replace empty string with NA
 Df_Huurderoordeel <- droplevels.data.frame(Df_Huurderoordeel) #Drop empty levels
 Df_Huurderoordeel[,c(6:10)] <- lapply(Df_Huurderoordeel[,c(6:10)], function(x) as.numeric(gsub(",", ".", x)))
@@ -70,14 +77,57 @@ Df_Onderhoud$Referentiewaarde.technische.woningkwaliteit..score. <- as.numeric(g
 Df_Onderhoud$Referentiewaarde.ervaren.woningkwaliteit..score. <- as.numeric(gsub(",", ".", gsub("\\.", "",Df_Onderhoud$Referentiewaarde.ervaren.woningkwaliteit..score.))) #rapportcijfer als numeriek
 Df_Onderhoud$Ervaren.woningkwaliteit..score. <- as.numeric(gsub(",", ".", gsub("\\.", "",Df_Onderhoud$Ervaren.woningkwaliteit..score.))) #rapportcijfer als numeriek
 
+#----------------------------
+#----------------------------
+#----------------------------
+# Deterministic (real) method
+#----------------------------
+#----------------------------
+#----------------------------
+
+#Prepare model matrix
+Df_Huurderoordeel_jaar <- Df_Huurderoordeel %>% filter(Jaar == 2019) ################## Change for different year
+
+Huurderoordeel_determ <- Df_Huurderoordeel_jaar %>% dplyr::select(Corporatie,Letter.huurdersoordeel..Letters..3.klasse..,Deelletter.huurders.met.een.reparatieverzoek,Deelletter.nieuwe.huurders..Letters..3.klasse..,Deelletter.vertrokken.huurders..Letters..3.klasse..) %>%
+  mutate(
+    Score_R = case_when(
+      Deelletter.huurders.met.een.reparatieverzoek == "A" ~ 1*0.55,
+      Deelletter.huurders.met.een.reparatieverzoek == "B" ~ 0*0.55,
+      Deelletter.huurders.met.een.reparatieverzoek == "C" ~ -1*0.55
+    )
+  ) %>%
+  mutate(
+    Score_VH = case_when(
+      Deelletter.vertrokken.huurders..Letters..3.klasse.. == "A" ~ 1*0.2,
+      Deelletter.vertrokken.huurders..Letters..3.klasse.. == "B" ~ 0*0.2,
+      Deelletter.vertrokken.huurders..Letters..3.klasse.. == "C" ~ -1*0.2
+    )
+  ) %>%
+  mutate(
+    Score_NH = case_when(
+      Deelletter.nieuwe.huurders..Letters..3.klasse.. == "A" ~ 1*0.25,
+      Deelletter.nieuwe.huurders..Letters..3.klasse.. == "B" ~ 0*0.25,
+      Deelletter.nieuwe.huurders..Letters..3.klasse.. == "C" ~ -1*0.25
+    )
+  ) %>% rowwise() %>% mutate(Weging = sum(Score_R,Score_VH,Score_NH, na.rm=TRUE)) %>% dplyr::select(Corporatie, Letter.huurdersoordeel..Letters..3.klasse..,Weging) %>% 
+  arrange(desc(Weging))
+
+Huurderoordeel_determ <- Huurderoordeel_determ[complete.cases(Huurderoordeel_determ),]  #Mozaiek Wonen, Pr Wonen, Sit. Woondiensten 
+
+names(Huurderoordeel_determ) <- c("Corporatie","Y","Weging")
+
+# Boxplot
+ggplot(Huurderoordeel_determ, aes(x=Y, y=Weging, fill=Y)) + 
+  geom_boxplot() + ylim(-1,1) + theme_bw() #Alleen in 2019 wat "onverklaarbare overlap tussen B en C"
+
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
-# Prepare Model Matrix
+# Prepare Model Matrix 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
 #Prepare model matrix
-Df_Huurderoordeel_jaar <- Df_Huurderoordeel %>% filter(Jaar == 2019) #Change for different year
+Df_Huurderoordeel_jaar <- Df_Huurderoordeel %>% filter(Jaar == 2019) ################## Change for different year
 prop.table(table(Df_Huurderoordeel_jaar$Letter.huurdersoordeel..Letters..3.klasse..)) #size per class
 names(Df_Huurderoordeel_jaar[c(5,6,12,18)]) # Predictors & Repsonse variables
 Model_Df <- Df_Huurderoordeel_jaar[,c(5,6,12,18)]
@@ -103,8 +153,8 @@ cor(Model_Df[,c(2:4)])
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
-# Fit the Conceptual Benchmark model Huurdersoordeel (should be 100% correct)
-# Random Forest
+# Fit model Huurdersoordeel 
+# Rpart & Random Forest
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
@@ -125,9 +175,9 @@ rf <- randomForest(
   Y ~ .,
   data=Model_Df, importance=TRUE
 )
-rf #5 Misclassified. Not exactly 100%. How?  
+rf #5 Misclassified. Bijna perfect zonder kennis van "weging"   
 
-View(rf$confusion)
+#View(rf$confusion)
 varImpPlot(rf, type=1)
 varImpPlot(rf, type=2)
 
@@ -138,17 +188,17 @@ for (i in 1:length(Model_Df$Y)) { if (Model_Df$Y[i] == rf$predicted[i]) { indice
 }
 Check_False <- Model_Df[which(!indices),]
 Check_False$Predicted <- rf$predicted[which(!indices)] #Vertaling van Deelletter naar eindscore?
-View(Check_False)
+#View(Check_False)
 
 # Fit SVM
-SupVecMach = svm(Y ~ .,
+#SupVecMach = svm(Y ~ .,
                  data = Model_Df)
-summary(SupVecMach)
+#summary(SupVecMach)
 
 # Confusion Matrix
-fit.val <- predict(SupVecMach, type = "class")
-cm <- table(actual = Model_Df$Y, fitted = fit.val)
-cm #Worse than RF
+#fit.val <- predict(SupVecMach, type = "class")
+#cm <- table(actual = Model_Df$Y, fitted = fit.val)
+#cm #Worse than RF
 
 #-----
 #Plots
@@ -169,49 +219,6 @@ Long <- gather(Model_Df,Deelmeting,Score,X_NH:X_VH, factor_key=TRUE)
 ggplot(Long, aes(x = jitter(Score), y = Deelmeting, fill = Y)) + geom_boxplot(alpha=.2) + labs(x = "Rapportcijfer") + theme_bw() + theme(legend.position = "none")
 
 
-#-----------------------------------------------------------
-# Prepare again with different Letter deelscore as predictor
-#-----------------------------------------------------------
-
-#Prepare model matrix
-#Df_Huurderoordeel_jaar <- Df_Huurderoordeel %>% filter(Jaar == 2020) #Change for different year
-#prop.table(table(Df_Huurderoordeel_jaar$Letter.huurdersoordeel..Letters..3.klasse..)) #size per class
-#names(Df_Huurderoordeel_jaar[c(5,6,12,18)]) # Predictors & Repsonse variables
-#Model_Df <- Df_Huurderoordeel_jaar[,c(5,11,22,40)]
-#names(Model_Df) <- c("Y","X_NH","X_VH","X_R") #Rename for readability 
-#row.names(Model_Df) <- Df_Huurderoordeel_jaar$Corporatie #Corporatienaam Rownames 
-
-#Deal with missing
-#table(complete.cases(Model_Df)) # 269 #126 incomplete. Meeste missen alle waarden. Sommige enkele en hebben desondanks wel een score.
-#Model_Df <- Model_Df[-c(which(is.na(Model_Df$Y))),] #Use only cases with Y score. Drop empty rows 
-#which(is.na(Model_Df$X_VH)) # 62 en 172 missing predictor
-#Model_Df[-c(which(complete.cases(Model_Df$X_VH))),]
-
-#Impute missing 
-#imp <- mice(Model_Df, m=1, maxit = 50, method = 'pmm', seed = 500)
-#summary(imp)
-#imp$imp$X_VH #Imputed values
-#Model_Df <- complete(imp)
-#Model_Df[-c(which(complete.cases(Model_Df$X_VH))),]
-
-#---------------------------------------------------------------------------
-#---------------------------------------------------------------------------
-# Fit the Conceptual Benchmark model Huurdersoordeel (should be 100% correct)
-# Random Forest
-#---------------------------------------------------------------------------
-#---------------------------------------------------------------------------
-
-# Fit tree model
-#set.seed(100481)
-#Tree <- rpart(Y~., data=Model_Df)
-#rpart.plot(Tree, box.palette="RdBu", shadow.col="gray", nn=TRUE)
-#summary(Tree)
-
-# Confusion Matrix
-#fit.val <- predict(Tree, type = "class")
-#cm <- table(actual = Model_Df$Y, fitted = fit.val)
-#cm
-
 #-------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------
 #Merge on Year and L-nummer Large DF new model_Df without previous huurdersoordeel predictors vars
@@ -231,12 +238,23 @@ Df <- merge(Df, Df_Onderhoud[,-c(2,3,4)], by="Rec_ID") #duurzaamheid, beschikbaa
 #Check Duplicates
 table(duplicated(Df)) #merge created duplicates.
 Df <- unique(Df) #Keep unique
+names(Df)
 
-#---------------
-#Analyse Missing
-#---------------
 
-Miss <- Df[,-c(27,34:39,113:116)] # >0.5 missing
+#----------------------
+#Analyse/Handle Missing
+#----------------------
+
+nrow(Df) #1143 
+Df <- Df[which(rowSums(is.na(Df))<50),] #Keep rows with less than 50 missing. Te kort door de bocht. Imputatie per variabele. Voor nu niet haalbaar. 
+nrow(Df) #Left 818
+
+ncol(Df) #119
+sort(colSums(is.na(Df)))
+Df <- Df[,which(colSums(is.na(Df))<160)] #idem.
+ncol(Df) #84
+
+Miss <- Df 
 names(Miss) <- c(1:length(names(Miss))) #give numbers as column names for plot readability
 names(Miss)
 
@@ -244,9 +262,23 @@ Missing_plot <- aggr(Miss, col=c('navyblue','yellow'),
                     numbers=TRUE, sortVars=TRUE,
                     labels=names(Miss), cex.axis=.5,
                     gap=3, ylab=c("Missing data","Pattern"))
-Missing_plot #What to do?
+Missing_plot 
 
-Df <- Df[,-c(27,34:39,113:116)] # Keep <0.5 missing
+#KNN Imputation remaining
+
+Df <- kNN(Df, k=10) # With K as 10
+
+#plot missing again. 
+
+Miss <- Df 
+names(Miss) <- c(1:length(names(Miss))) #give numbers as column names for plot readability
+names(Miss)
+
+Missing_plot <- aggr(Miss, col=c('navyblue','yellow'),
+                     numbers=TRUE, sortVars=TRUE,
+                     labels=names(Miss), cex.axis=.5,
+                     gap=3, ylab=c("Missing data","Pattern"))
+Missing_plot 
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
@@ -260,46 +292,23 @@ names(Model_Df2)[5] <- "Y"
 row.names(Model_Df2) <- Model_Df2$Corporatie #Corporatienaam Rownames 
 Model_Df2 <- Model_Df2[,-c(1:4)]
 
-#Deal with missing
-Miss <- Model_Df2[] 
-names(Miss) <- c(1:length(names(Miss))) #give numbers as column names for plot readability
-
-
-Missing_plot <- aggr(Miss, col=c('navyblue','yellow'),
-                     numbers=TRUE, sortVars=TRUE,
-                     labels=names(Miss), cex.axis=.5,
-                     gap=3, ylab=c("Missing data","Pattern"))
-Missing_plot #What to do?
-Missing_plot
-
-names(Model_Df2)[c(9,15,21,22,23,24,25)] #> 50% missing
-
-Model_Df2 <- Model_Df2[,-c(9,15,21,22,23,24,25)] #Do more? ##########################################
-Model_Df3 <- na.exclude(Model_Df2) # Te kort door de bocht ##########################################
-
-#Impute missing 
-#imp <- mice(Model_Df, m=1, maxit = 50, method = 'pmm', seed = 500)
-#summary(imp)
-#imp$imp$X_VH #Imputed values
-#Model_Df <- complete(imp)
-#Model_Df[-c(which(complete.cases(Model_Df$X_VH))),]
-
 #----------
 # Fit model
 #----------
 
-# Remove Huurdersooordeel Predictor Variables
-names(Model_Df3[c(2:22,72,73)])
-Model_Df4 <- Model_Df3[,-c(2:22,72,73)] 
+# Remove Huurdersoordeel Predictor Variables
+names(Model_Df2)
+Model_Df2 <- Model_Df2[,-c(2,3,4,5,6,7,8,9)] 
 
 #Fit Random Forest
-set.seed(0)
+set.seed(2000)
 rf2 <- randomForest(
   Y ~ .,
-  data=Model_Df4, importance=TRUE
+  data=Model_Df2, importance=TRUE
 )
 rf2
-View(rf2$confusion)
+
+#View(rf2$confusion)
 varImpPlot(rf2, n.var = 10, type=1) # Roughly 60%
 ?varImpPlot
 
@@ -322,11 +331,14 @@ View(Check_False[,c("Y","Predicted")]) # Welke?
 
 # Prepare 
 Df2019 <- Df %>% dplyr::filter(Jaar == "2019") #work with 2019 data
+Df2019 <- Df2019[!duplicated(Df2019$Corporatie),]
 row.names(Df2019) <- Df2019$Corporatie
-Df2019 <- Df2019[complete.cases(Df2019),] # only complete cases
+
+#Df2019 <- Df2019[complete.cases(Df2019),] # only complete cases
 nums <- unlist(lapply(Df2019, is.numeric))  
-Df2019 <- Df2019[ ,nums] #Only numeric vars. 90 variables remaining
-Df2019 <- Df2019[,-c(1,27,62,63,64,65,67)] # remove near zero variance vars
+Df2019 <- Df2019[ ,nums] #Only numeric vars. 66 variables remaining
+which(summarise_if(Df2019,is.numeric, var)==0) #detect zero variance
+Df2019 <- Df2019[,-c(which(summarise_if(Df2019,is.numeric, var)==0))] # remove near zero variance vars
 
 # PCA
 df.pca <- prcomp(Df2019, center = TRUE,scale. = TRUE) #scale en center
